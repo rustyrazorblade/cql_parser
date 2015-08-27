c59bd0b0118b4910cb6ceddc9e2c594b7e572681
Jon Haddad <jon@jonhaddad.com>
2015-08-26 17:22:31 -0700

    added a ton of tests from the cql 3 docs

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,6 +5,9 @@ use cql_parser::cql::parse;
 #[test]
 fn test_legal_queries() {
     let queries = include_str!("passing.cql").split("\n");
+    let mut failed = 0;
+    let mut passed = 0;
+    let mut failed_queries = Vec::new();
 
     for query in queries {
         // skip blank lines and lines that are a comment
@@ -16,11 +19,20 @@ fn test_legal_queries() {
         }
         let parsed = parse(query);
         match parsed {
-            Ok(stmt) => (),
+            Ok(stmt) => {
+                passed += 1;
+                },
             Err(x) => {
-                panic!("Error parsing {}", query);
+                failed += 1;
+                failed_queries.push(query);
             }
         };
+    }
+    if failed > 0 {
+        for query in failed_queries {
+            println!("{}", query);
+        }
+        panic!("Failed tests: {} of {}", failed, passed + failed);
     }
 
 }
